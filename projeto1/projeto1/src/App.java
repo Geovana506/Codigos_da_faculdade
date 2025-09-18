5c2adff925bafac3814efa3cc6a54504da204e55
Geovana506 <geovanacarvalho506@gmail.com>
2025-09-17 23:16:33 -0300

    inicio

--- projeto1/projeto1/src/App.java
+++ projeto1/projeto1/src/App.java
@@ -1,5 +1,36 @@
+import java.time.LocalDate;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Produto produto = new Produto();
+        produto.setNome("Camiseta");        
+        produto.setMarca("Nike");
+        produto.setPreco(79.90);
+        produto.setQtde(10);
+
+        Produto produto1 = new Produto();
+        produto1.setNome("Jaqueta");        
+        produto1.setMarca("Vlone");
+        produto1.setPreco(799.90);
+        produto1.setQtde(12);
+       
+        Venda venda = new Venda();
+        venda.setData(LocalDate.now());
+        venda.setNumeroVenda(1123);
+        venda.setProduto(produto);
+
+        System.out.println("Data da venda: " + venda.getData());
+        System.out.println("Número da venda: " + venda.getNumeroVenda());
+
+        System.out.println("====================DADOS DO PRODUTO====================");
+        System.out.println("Produto Vendido: " + venda.getProduto().getNome());
+        System.out.println("Marca: " + venda.getProduto().getMarca());
+        System.out.println("Preço: " + venda.getProduto().getPreco());
+        System.out.println("Quantidade em estoque: " + venda.getProduto().getQtde());
+        System.out.println("========================================================");
+
+
+
     }
-}
+
+ }
---
+++ projeto1/projeto1/src/Venda.java
@@ -0,0 +1,46 @@
+import java.time.LocalDate;
+
+public class Venda {
+    private LocalDate data;
+    private Double valorTotal;
+    private Produto produto;
+    private Integer numeroVenda;
+
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public Double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(Double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public int getNumeroVenda() {
+        return numeroVenda;
+    }
+
+    public void setNumeroVenda(int numeroVenda) {
+        this.numeroVenda = numeroVenda;
+    }
+
+    public void setNumeroVenda(Integer numeroVenda) {
+        this.numeroVenda = numeroVenda;
+    }
+
+}
